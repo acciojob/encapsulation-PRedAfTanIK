65eea11d32160388fafc7f55ea8fa88abaa2ce8c
PRedAfTanIK <predaftanik0@gmail.com>
2023-03-18 13:38:43 +0530

    Encap Ass

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.val = 10;
+        //System.out.println(obj.val);
+        //java: val has private access in com.driver.RWOnly
+        obj.setVal(10);
+        System.out.println(obj.getVal());
+    }
 }
